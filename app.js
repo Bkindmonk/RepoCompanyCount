bf84d9a60d5c677620f98b83a9c34c8f82a7263d
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-08 01:06:57 -0600

    added parallel requests with a callback function that happens once all of them return

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var request = require('request');
 var async = require('async');
 var contributors_list = 'https://api.github.com/repos/cla-assistant/cla-assistant/contributors';
-
+var callback_count = 0;
 
 //connection object to house the headers, as the GitHub server requires it
 var connection = {
@@ -17,8 +17,7 @@ var connection = {
 //without changing the main function, creating less confusion over multiple iterations,
 //and creating more readable code
 function company_dictionary() { };
-
-company_dictionary.countCompany = function (company_name) {
+function countCompany(company_name) {
     //if the company doesn't exist already in the dictionary, create it
     if (!(company_name in company_dictionary)) {
         company_dictionary[company_name] = 0;
@@ -27,10 +26,11 @@ company_dictionary.countCompany = function (company_name) {
     company_dictionary[company_name] += 1;
 }
 
-company_dictionary.getCompanyCount = function (company_name) {
+function getCompanyCount (company_name) {
     return company_dictionary[company_name];
 }
 
+/*
 function getCompany(contributor_url) {
     var contributor_connection = {
         url: contributor_url,
@@ -38,8 +38,10 @@ function getCompany(contributor_url) {
             'User-Agent': 'request'
         }
     }
+    
     //request the company for each contributor
     request(contributor_connection, function(error, response, body){
+        
         //if there was no error in the communication or request, proceed
         if(!error && response.statusCode ==200){
             //parse the response into a JSON object
@@ -48,19 +50,23 @@ function getCompany(contributor_url) {
             var company = profile['company'];
             //if the comapny is null, return unknown
             if(company == null){
-                return 'unknown'
+                countCompany('unknown');
+                callback_count +=1;
             }else{
-                return company;
+                countCompany(company);
+                callback_count +=1;
             }
         }else{
             console.error('error for '+contributor_url)
-            //in case of communication error, return "error". An unlikely comany name.
-            //Given an error count in the output. Error log can be 
-            return 'error';
+            callback_count +=1;
+            //in case of communication error, print it to the console for debugging
         }
+        
     });
-}
 
+    
+}
+*/
 //main logic
 request(connection, function (error, response, body) {
 
@@ -68,19 +74,21 @@ request(connection, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         //parse the response into a JSON object
         var contributors = JSON.parse(body);
-
-
+        
+        //a list to store the async calls that need to be executed in parallel
         var contributor_task_list = [];
-
-        //construct the async tasks for all request to be processed in parallel
-        contributors.forEach(function(contributor_index){
-            var contributor_url = contributors[contributor_index]['url']
+        
+        //get the company for each entry and add them to the dictionary
+        for (var contributor_data in contributors) {
+            var contributor_url = contributors[contributor_data]['url']
+            
             var contributor_connection = {
                 url: contributor_url,
                 headers: {
                     'User-Agent': 'request'
                 }
             }
+
             contributor_task_list.push(function(callback){
                 request(contributor_connection, function(error, response, body){
                     //if there was no error in the communication or request, proceed
@@ -89,41 +97,34 @@ request(connection, function (error, response, body) {
                         var profile = JSON.parse(body);
                         //get the company name
                         var company = profile['company'];
-                        //if the comapny is null, return unknown
+                        console.log(company);
+                        //if the comapny is null, count unknown
                         if(company == null){
-                            return 'unknown'
+                            countCompany('unknown');
                         }else{
-                            return company;
+                            countCompany(company);
                         }
                     }else{
                         console.error('error for '+contributor_url)
-                        //in case of communication error, return "error". An unlikely comany name.
-                        //Given an error count in the output. Error log can be 
-                        return 'error';
+                        //in case of communication error, print it to the console for debugging
                     }
                 });
+                //lets async know it is done
                 callback();
             });
-        });
+            
+        }
 
-        async.parallel(contributor_task_list, function(err, company_list){
-            for (var comapny_name in company_list) {
-                company_dictionary.countCompany(comapny_name);
+        async.parallel(contributor_task_list, function(error,results){
+            //after all the async calls have finished
+            //for each entry in the dictionary print the count
+            for (var comapny_name in company_dictionary) {
+                var company_count = getCompanyCount(comapny_name);
+                console.log(comapny_name + " " + company_count);
             }
         });
-/*
-        //get the company for each entry and add them to the dictionary
-        for (var contributor_data in contributors) {
-            var company = getCompany(contributors[contributor_data]['url']);
-            console.log(contributors[contributor_data]['url']);
-            //count the company
-            company_dictionary.countCompany(company);
-        }
-*/
-        //for each entry in the dictionary print the count
-        for (var comapny_name in company_dictionary) {
-            console.log(comapny_name + " " + company_dictionary.getCompanyCount(comapny_name));
-        }
+
+        
 
     }else{
         //if there was an error with the connection, print the error and result code
