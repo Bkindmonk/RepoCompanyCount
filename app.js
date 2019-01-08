e103720a0a90ca89a29ab172b141169a1effa748
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-07 21:53:20 -0600

    added header to request as the gitHub server requires it

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 'use strict';
-
+var request = require('request');
 var contributors_list = 'https://api.github.com/repos/cla-assistant/cla-assistant/contributors';
+
+var connection = {
+    url: contributors_list,
+    headers: {
+        'User-Agent': 'request'
+    }
+};
+
 
 //making it an object, allows us to change the datastructure for the company counts, 
 //without changing the main function, creating less confusion over multiple iterations,
 //and creating more readable code
 function company_dictionary() { };
 
-company_dictionary.prototype.countCompany = function (company_name) {
+company_dictionary.countCompany = function (company_name) {
     //if the company doesn't exist already in the dictionary, create it
     if (!(company_name in company_dictionary)) {
         company_dictionary[company_name] = 0;
@@ -16,21 +24,22 @@ company_dictionary.prototype.countCompany = function (company_name) {
     company_dictionary[company_name] += 1;
 }
 
-company_dictionary.prototype.getCompanyCount = function (company_name) {
+company_dictionary.getCompanyCount = function (company_name) {
     return company_dictionary[company_name];
 }
 
 //main logic
-request(contributors_list, function (error, response, body) {
+request(connection, function (error, response, body) {
+ 
     //if there was no error in the communication or request, proceed
     if (!error && response.statusCode == 200) {
         //parse the response into a JSON object
         var contributors = JSON.parse(body);
 
         //get the company for each entry and add them to the dictionary
-
+        company_dictionary.countCompany("test1");
         //for each entry in the dictionary print the count
-
+        console.log(company_dictionary.getCompanyCount("test1"));
         
     }
 });
