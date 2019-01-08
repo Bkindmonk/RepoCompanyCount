01a85c2f0330ef6eeca4843e4b1807c80726ec70
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-07 22:39:11 -0600

    implemented getCompany and reached access limit for github

--- app.js
+++ app.js
@@ -31,12 +31,38 @@ company_dictionary.getCompanyCount = function (company_name) {
 }
 
 function getCompany(contributor_url) {
+    var contributor_connection = {
+        url: contributor_url,
+        headers: {
+            'User-Agent': 'request'
+        }
+    }
     //request the company for each contributor
+    request(contributor_connection, function(error, response, body){
+        //if there was no error in the communication or request, proceed
+        if(!error && response.statusCode ==200){
+            //parse the response into a JSON object
+            var profile = JSON.parse(body);
+            //get the company name
+            var company = profile['comapny'];
+            //if the comapny is null, return unknown
+            if(company == null){
+                return 'unknown'
+            }else{
+                return company;
+            }
+        }else{
+            console.error('error for '+contributor_url)
+            //in case of communication error, return "error". An unlikely comany name.
+            //Given an error count in the output. Error log can be 
+            return 'error';
+        }
+    });
 }
 
 //main logic
 request(connection, function (error, response, body) {
- 
+
     //if there was no error in the communication or request, proceed
     if (!error && response.statusCode == 200) {
         //parse the response into a JSON object
@@ -44,16 +70,19 @@ request(connection, function (error, response, body) {
 
         //get the company for each entry and add them to the dictionary
         for (var contributor_data in contributors) {
-            var company = getCompany(contributor_data['url']);
-
+            var company = getCompany(contributors[contributor_data]['url']);
+            console.log(contributors[contributor_data]['url']);
             //count the company
-            comapny_dictionary.countCompany(company);
+            company_dictionary.countCompany(company);
         }
 
         //for each entry in the dictionary print the count
-        for (var comapny_name in comapny_dictionary) {
-            console.log(comapny_name + " " + comapny_dictionary.getCompanyCount(comapny_name));
+        for (var comapny_name in company_dictionary) {
+            console.log(comapny_name + " " + company_dictionary.getCompanyCount(comapny_name));
         }
         
+    }else{
+        console.log(response.statusCode);
+        console.log(response.body);
     }
 });
