9f59d0e0f700b76417db43c9d0e98b24dcb958f4
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-07 21:28:11 -0600

    basic idea laid out

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('Hello world');
+var contributors_list = 'https://api.github.com/repos/cla-assistant/cla-assistant/contributors';
+
+//making it an object, allows us to change the datastructure for the company counts, 
+//without changing the main function, creating less confusion over multiple iterations,
+//and creating more readable code
+function company_dictionary() { };
+
+
+
+//main logic
+request(contributors_list, function (error, response, body) {
+    //if there was no error in the communication or request, proceed
+    if (!error && response.statusCode == 200) {
+        //parse the response into a JSON object
+        var contributors = JSON.parse(body);
+
+        //get the company for each entry and add them to the dictionary
+
+        //for each entry in the dictionary print the count
+
+        
+    }
+});
